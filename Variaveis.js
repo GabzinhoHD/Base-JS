9048a9c6e7edf18ada84c25432486a40cc0dd312
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-08-14 09:28:41 -0300

    basess

---
+++ Variaveis.js
@@ -0,0 +1,18 @@
+// variavel
+// tipo Number
+
+const meuNumero = 78;
+const numeroUm = 19;
+const numeroDois = 98;
+
+const soma = numeroDois + numeroUm;
+console.log(soma)
+
+const subtracao = numeroUm - numeroDois;
+console.log(subtracao)
+
+const Multiplica = numeroDois * meuNumero;
+console.log(Multiplica)
+
+const divisao = meuNumero / numeroDois;
+console.log(divisao)
